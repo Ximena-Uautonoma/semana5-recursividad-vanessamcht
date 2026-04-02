29e6a307aec6a99c6e49a7c598d97f574abcd456
vanessamcht <vanessa.marchant@cloud.uautonoma.cl>
2026-04-02 00:08:28 -0300

    hola2

--- Semana5/ejercicio2.py
+++ Semana5/ejercicio2.py
@@ -9,8 +9,14 @@ Debe implementar:
 
 def suma_ciclo(n):
     #retorna la suma de los primeros n numeros usando un ciclo 
-
+    suma = 0 
+    for i in range (1, n + 1 ):
+        suma = suma + i 
+        return suma
 
 def suma_recursiva(n):
     #retorna la suma de los primeros n números usando recursividad 
-    pass
+    if n == 1: 
+        return 1
+    else: 
+        return suma_recursiva(n-1)+n

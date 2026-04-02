919b9fa9351ab34894de2ad31d7ee2e6f33f2ac0
vanessamcht <vanessa.marchant@cloud.uautonoma.cl>
2026-04-02 00:28:26 -0300

    hola2

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,17 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
+    resultado = 1 
+    contador = 1 
+    while contador <= n: 
+        resultado = resultado * contador
+        contador = contador + 1
+    return resultado
+   
 
 
 def factorial_recursivo(n):
-    pass
+    if n == 1: 
+        return 1
+    else: 
+        return n * factorial_recursivo(n-1)
